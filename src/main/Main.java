acf001f9e405497b64f2c24fd8a16bb690d7c2e5
Ivan <ivan90_bg@hotmail.com>
2018-06-22 16:09:40 +0200

    expands class student and tests all classes in main method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,26 @@
 package main;
+
+import student.Student;
+
 public class Main {
 
     
     public static void main(String[] args) {
+        
+    Student marko = new Student();
+    marko.setFirstName("Igor");
+    marko.setLastName("Markovic");
+    marko.setYearOfBirth(1987);
+    marko.getCourse().setName("engleski");
+    marko.getCourse().setNumberOfClasses(88);
+    marko.getComputer().setProcessTact(2.7);
+    marko.getComputer().setMemory(22.5);
+    marko.getComputer().setHardDrive(100);
+    marko.info();
+        
+        
+        
+        
         
     }
     
--- src/student/Student.java
+++ src/student/Student.java
@@ -1,4 +1,3 @@
-
 package student;
 
 import computer.Computer;
@@ -6,24 +5,26 @@ import course.Course;
 
 public class Student {
 
-private String firstName;
-private String lastName;
-private int yearOfBirth;
-private Course course;
-private Computer computer;
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
+    private Course course;
+    private Computer computer;
 
     public Student() {
         this.firstName = "ime";
         this.lastName = "prezime";
         this.yearOfBirth = 0;
-        this.course=new Course();
-        this.computer=new Computer();
+        this.course = new Course();
+        this.computer = new Computer();
     }
 
-    public Student(String firstName, String lastName, int yearOfBirth) {
+    public Student(String firstName, String lastName, int yearOfBirth, Course course, Computer computer) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.yearOfBirth = yearOfBirth;
+        this.course = course;
+        this.computer = computer;
     }
 
     public String getFirstName() {
@@ -58,11 +59,16 @@ private Computer computer;
         return computer;
     }
 
-public void info(){
-    System.out.println("First name: " +getFirstName());
-    System.out.println("Last name: "+getLastName());
-    System.out.println("Year of birth: "+getYearOfBirth());
+    public void info() {
+        System.out.println("First name: " + getFirstName());
+        System.out.println("Last name: " + getLastName());
+        System.out.println("Year of birth: " + getYearOfBirth());
+        System.out.println("Course name: " + getCourse().getName());
+        System.out.println("Course number of classes: " + getCourse().getNumberOfClasses());
+        System.out.println("Process tact: " + getComputer().getProcessTact());
+        System.out.println("Memory: " + getComputer().getMemory());
+        System.out.println("Hard drive: " + getComputer().getHardDrive());
 
-}
-    
+    }
+
 }
